109375c83cbd9ccaeb8f922cd15d6ff68b0b0570
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-06 01:45:00 +0900

    Update 3 도서추천알고리즘.py

--- pages/3 도서추천알고리즘.py
+++ pages/3 도서추천알고리즘.py
@@ -80,17 +80,28 @@ user_based_cf.fit(trainset)
 def recommend_books(book_title):
     book_rating = pivot_data[book_title]
     similar_books_index = cos_sim[np.argsort(book_rating)][-6:-1]
-    similar_books = list(pivot_data.columns[similar_books_index])
+    similar_books = list(pivot_data.index[similar_books_index])
     recommended_books = []
     for book in similar_books:
         _, _, _, est, _ = user_based_cf.predict(uid=book, iid=book_title)
         if est >= 4.0:
             recommended_books.append(book)
-    recommended_books.append(book_title)
     return recommended_books
+
 
 # Streamlit 앱 구성
 st.title('Book Recommender')
-book
+book_title = st.text_input('Enter a book title')
+if book_title in pivot_data.columns:
+    recommended_books = recommend_books(book_title)
+    if len(recommended_books) > 0:
+        st.write('Recommended books:')
+        for book in recommended_books:
+            st.write('- ' + book)
+    else:
+        st.write('No recommended books')
+else:
+    st.write('Enter a valid book title')
+
 
 
