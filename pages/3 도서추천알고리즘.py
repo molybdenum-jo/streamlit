f100dabd22f0417b4a78f9546079f02ae6776b8a
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-06 17:35:52 +0900

    Update 3 도서추천알고리즘.py

--- pages/3 도서추천알고리즘.py
+++ pages/3 도서추천알고리즘.py
@@ -297,22 +297,20 @@ def recommend_books(book_title):
     svd_similar_books = list(pivot_data.columns[svd_similar_books_index])
     
     # Item-based 모델
-    book_title_idx = count_vect.get_feature_names().index(book_title)
-    item_similar_books_index = np.unique(np.argsort(book_title_sim[:, book_title_idx])[-6:-1])
+    book_title_matrix = count_vect.transform([book_title])
+    book_title_sim = cosine_similarity(book_title_matrix, book_title_matrix.T)
+    item_similar_books_index = np.unique(np.argsort(book_title_sim[:, 0])[-6:-1])
     item_similar_books = list(train['Book-Title'][item_similar_books_index])
 
     # 사용자가 선택한 책과 유사한 책 5개 추천
-    def recommend_books(book_title):
-        book_rating = pivot_data[book_title]
-        similar_books_index = np.unique(np.argsort(cos_sim[pivot_data.columns.get_loc(book_title)])[-6:-1])
-        similar_books = list(pivot_data.columns[similar_books_index])
-        recommended_books = []
-        for book in similar_books:
-            _, _, _, est, _ = svd_model.predict(uid=book_title, iid=book)
-            if est >= 4.0:
-                recommended_books.append(book)
-        return recommended_books
-
+    similar_books = list(set(svd_similar_books + item_similar_books))
+    similar_books = [book for book in similar_books if book != book_title]
+    recommended_books = []
+    for book in similar_books:
+        _, _, _, est, _ = svd_model.predict(uid=book_title, iid=book)
+        if est >= 4.0:
+            recommended_books.append(book)
+    return recommended_books
 
 # Streamlit 앱 구성
 st.title('Book Recommender')
