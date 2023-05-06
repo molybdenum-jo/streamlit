a12591871e42586e76ad381d2352e2ee89b89f14
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-06 16:41:36 +0900

    Update 3 도서추천알고리즘.py

--- pages/3 도서추천알고리즘.py
+++ pages/3 도서추천알고리즘.py
@@ -243,8 +243,17 @@ js = "window.scrollTo(0, document.getElementById('part-5-book').offsetTop);"
 
 st.markdown("<h3 id='part-5-book'>✅Part 5. 앙상블 기법을 사용한 추천 시스템</h3>", unsafe_allow_html=True)
 
+import pandas as pd
+import numpy as np
+from surprise import Reader, Dataset, SVD
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
 # 데이터 불러오기
 train = pd.read_csv('data/TRAIN.csv')
+
+# 평점이 4점 이상인 데이터만 사용
+train = train[train['Book-Rating'] >= 4]
 
 # 사용자-아이템 행렬 생성
 pivot_data = train.pivot_table(index='User-ID', columns='Book-Title', values='Book-Rating', fill_value=0)
@@ -262,6 +271,7 @@ count_vect = CountVectorizer()
 book_title_matrix = count_vect.fit_transform(train['Book-Title'])
 book_title_sim = cosine_similarity(book_title_matrix)
 
+# 모델 합치기
 def recommend_books(book_title):
     book_rating = pivot_data[book_title]
     
@@ -277,17 +287,7 @@ def recommend_books(book_title):
     
     # 두 모델 결과 합치기
     similar_books = list(set(svd_similar_books + item_similar_books))
-    return similar_books
-
-
     
-
-
-# 사용자가 선택한 책과 유사한 책 5개 추천
-def recommend_books(book_title):
-    book_rating = pivot_data[book_title]
-    similar_books_index = np.unique(np.argsort(cos_sim[pivot_data.columns.get_loc(book_title)])[-6:-1])
-    similar_books = list(pivot_data.columns[similar_books_index])
     recommended_books = []
     for book in similar_books:
         _, _, _, est, _ = svd_model.predict(uid=book_title, iid=book)
@@ -296,6 +296,8 @@ def recommend_books(book_title):
     return recommended_books
 
 # Streamlit 앱 구성
+import streamlit as st
+
 st.title('Book Recommender')
 book_title = st.text_input('Enter a book title', key='input')
 if book_title in pivot_data.columns:
