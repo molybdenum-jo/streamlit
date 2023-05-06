9625e66e2f89c80c46beafbd71f466575b97fb6b
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-06 18:55:30 +0900

    Update 3 도서추천알고리즘.py

--- pages/3 도서추천알고리즘.py
+++ pages/3 도서추천알고리즘.py
@@ -324,6 +324,61 @@ from math import sqrt
 
 st.markdown("<h3 id='part-5-book'>✅Part 5. 앙상블 기법을 사용한 추천 시스템</h3>", unsafe_allow_html=True)
 
+# 데이터 불러오기
+train = pd.read_csv('data/TRAIN.csv')
+
+# 평점이 4점 이상인 데이터만 사용
+train = train[train['Book-Rating'] >= 4]
+
+# 사용자-아이템 행렬 생성
+pivot_data = train.pivot_table(index='User-ID', columns='Book-Title', values='Book-Rating', fill_value=0)
+
+# 데이터 나누기
+trainset, testset = train_test_split(train, test_size=0.2, random_state=42)
+
+# SVD 모델 구축
+reader = Reader(rating_scale=(1, 10))
+data = Dataset.load_from_df(train[['User-ID', 'Book-Title', 'Book-Rating']], reader)
+trainset = data.build_full_trainset()
+svd_model = SVD(n_factors=20, reg_all=0.02)
+svd_model.fit(trainset)
+
+# Item-based 앙상블 모델 구축
+# 책 제목 기반으로 벡터화
+count_vect = CountVectorizer()
+book_title_matrix = count_vect.fit_transform(train['Book-Title'])
+book_title_sim = cosine_similarity(book_title_matrix)
+
+svd_preds = svd_model.test(testset)
+item_preds = item_based_recommendation(user_id, book_title_sim)
+ensemble_preds = [(0.7 * svd_pred.est) + (0.3 * item_pred) for svd_pred, item_pred in zip(svd_preds, item_preds)]
+
+# 사용자가 선택한 책과 유사한 책 5개 추천
+def recommend_books(book_title):
+    book_rating = pivot_data[book_title]
+    similar_books = list(set(svd_similar_books + item_similar_books))
+    similar_books_index = np.unique(np.argsort(cos_sim[pivot_data.columns.get_loc(book_title)])[-6:-1])
+    similar_books = list(pivot_data.columns[similar_books_index])
+    recommended_books = []
+    for book in similar_books:
+        _, _, _, est, _ = svd_model.predict(uid=book_title, iid=book)
+        if est >= 4.0:
+            recommended_books.append(book)
+    return recommended_books
+
+# Streamlit 앱 구성
+st.title('Book Recommender')
+book_title = st.text_input('Enter a book title', key='input')
+if book_title in pivot_data.columns:
+    recommended_books = recommend_books(book_title)
+    if len(recommended_books) > 0:
+        st.write('Recommended books:')
+        for book in recommended_books:
+            st.write('- ' + book)
+    else:
+        st.write('No recommended books')
+else:
+    st.write('Enter a valid book title')
 
 js = "window.scrollTo(0, document.getElementById('part-6-book').offsetTop);"
 
