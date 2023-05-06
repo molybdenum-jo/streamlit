805d16b848448735e86e5f6d8f2c0d9963dd0322
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-06 14:53:15 +0900

    Update 3 도서추천알고리즘.py

--- pages/3 도서추천알고리즘.py
+++ pages/3 도서추천알고리즘.py
@@ -275,7 +275,7 @@ book_title_index = pivot_data.columns.get_loc(book_title)
 svd_similar_books_index = np.unique(np.argsort(cosine_similarity(pivot_data.iloc[:, :book_title_index].join(pivot_data.iloc[:, book_title_index+1:]), 
                                                                  pivot_data.iloc[:, book_title_index].values.reshape(1, -1)))[:, -6:-1].reshape(-1))
 svd_similar_books = list(pivot_data.columns[svd_similar_books_index])
-ot_data.columns[svd_similar_books_index])
+ot_data.columns[svd_similar_books_index]
     
 # Item-based 모델
 book_title_idx = count_vect.get_feature_names().index(book_title)
