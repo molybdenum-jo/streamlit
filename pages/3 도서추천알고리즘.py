7b7f6aba4fff100c82b3323ec7b1f1e44f35284c
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-06 19:28:29 +0900

    Update 3 도서추천알고리즘.py

--- pages/3 도서추천알고리즘.py
+++ pages/3 도서추천알고리즘.py
@@ -296,9 +296,8 @@ def predict_books(input_data):
 input_data = {'The Da Vinci Code': 
 
 
-
+js = "window.scrollTo(0, document.getElementById('part-5-book').offsetTop);"
 st.markdown("<h3 id='part-5-book'>✅Part 5. 앙상블 기법을 사용한 추천 시스템</h3>", unsafe_allow_html=True)
-
 
 
 js = "window.scrollTo(0, document.getElementById('part-6-book').offsetTop);"
