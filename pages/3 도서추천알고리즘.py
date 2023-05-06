0b22f58952d34ee73d567c6b63a2172cf0669f54
Molybdenum_jay <126542139+molybdenum-jo@users.noreply.github.com>
2023-05-06 11:09:13 +0900

    Update 3 도서추천알고리즘.py

--- pages/3 도서추천알고리즘.py
+++ pages/3 도서추천알고리즘.py
@@ -304,3 +304,77 @@ if book_title in pivot_data.columns:
         st.write('No recommended books')
 else:
     st.write('Enter a valid book title')
+
+
+js = "window.scrollTo(0, document.getElementById('part-5-book').offsetTop);"
+
+st.markdown("<h3 id='part-4-book'>✅Part 4. 앙상블 기법을 사용한 추천 시스템</h3>", unsafe_allow_html=True)
+
+import pandas as pd
+import numpy as np
+from surprise import Reader, Dataset
+from surprise import SVD
+from sklearn.metrics.pairwise import cosine_similarity
+
+# 데이터 불러오기
+train = pd.read_csv('data/TRAIN.csv')
+
+# 평점이 4점 이상인 데이터만 사용
+train = train[train['Book-Rating'] >= 4]
+
+# Book-ID에 고유한 정수 인덱스 부여
+unique_books = list(set(train['Book-ID']))
+book_to_idx = {book: i for i, book in enumerate(unique_books)}
+idx_to_book = {i: book for book, i in book_to_idx.items()}
+train['Book-ID'] = train['Book-ID'].map(book_to_idx)
+
+# 사용자-아이템 행렬 생성
+num_users = len(train['User-ID'].unique())
+num_books = len(train['Book-ID'].unique())
+ratings_matrix = np.zeros((num_users, num_books))
+for row in train.itertuples():
+    ratings_matrix[row[1]-1, row[3]] = row[2]
+
+# 딥러닝 모델 구축
+user_input = Input(shape=(1,))
+item_input = Input(shape=(1,))
+
+user_embedding = Embedding(num_users, 20)(user_input)
+user_vec = Flatten()(user_embedding)
+
+item_embedding = Embedding(num_books, 20)(item_input)
+item_vec = Flatten()(item_embedding)
+
+prod = Dot(name='Dot-Product', axes=1)([user_vec, item_vec])
+
+dense1 = Dense(64, activation='relu')(prod)
+dense2 = Dense(1)(dense1)
+
+model = Model([user_input, item_input], dense2)
+model.compile(loss='mse', optimizer='adam')
+
+model.fit([ratings_matrix[:, 0], ratings_matrix[:, 1]], ratings_matrix[:, 2], batch_size=128, epochs=10, validation_split=0.1)
+
+# 유사한 책 5개 추천
+def recommend_books(book_id):
+    book_idx = book_to_idx[book_id]
+    book_vec = model.get_layer('Embedding_2')(np.array([book_idx]))
+    sim_scores = cosine_similarity(book_vec, model.get_layer('Embedding_2').get_weights()[0])[0]
+    sim_books_idx = np.argsort(sim_scores)[-6:-1]
+    sim_books = [idx_to_book[i] for i in sim_books_idx]
+    return sim_books
+
+# Streamlit 앱 구성
+st.title('Book Recommender')
+book_title = st.text_input('Enter a book title', key='input')
+if book_title in pivot_data.columns:
+    recommended_books = recommend_books(book_title)
+    if len(recommended_books) > 0:
+        st.write('Recommended books:')
+        for book in recommended_books:
+            st.write('- ' + book)
+    else:
+        st.write('No recommended books')
+else:
+    st.write('Enter a valid book title')
+
